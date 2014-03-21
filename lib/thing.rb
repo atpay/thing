30fea5b1148c47688cab381c2bc6a14b9e414e2a
Glen <DamnBigMan@gmail.com>
2014-03-21 11:05:25 -0600

    Moving startup to bin/thing

--- lib/thing.rb
+++ lib/thing.rb
@@ -3,9 +3,3 @@ require 'yaml'
 require_relative 'thing/tab'
 require_relative 'thing/suite'
 require_relative 'thing/ben_grimm'
-
-URI = "druby://localhost:9797"
-
-DRb.start_service(URI, Thing::BenGrimm.new)
-
-DRb.thread.join
